3498f7feb26651beed3c672f05bb0c737d7da919
Bamcito <harrisonnoscue04@gmail.com>
2026-06-03 15:48:15 -0500

    integracion de prueba para la division

--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -2,22 +2,32 @@
 use PHPUnit\Framework\TestCase;
 use App\Calculadora;
 
-class CalculadoraTest extends TestCase {
+class CalculadoraTest extends TestCase
+{
     private $calc;
 
-    protected function setUp(): void {
+    protected function setUp(): void
+    {
         $this->calc = new Calculadora();
     }
 
-    public function testSuma() {
+    public function testSuma()
+    {
         $this->assertEquals(5, $this->calc->sumar(2, 3));
     }
 
-    public function testResta() {
+    public function testResta()
+    {
         $this->assertEquals(1, $this->calc->restar(4, 3));
     }
 
-    public function testMultiplicacion() {
+    public function testMultiplicacion()
+    {
         $this->assertEquals(12, $this->calc->multiplicar(4, 3));
+    }
+
+    public function testDivision()
+    {
+        $this->assertEquals(2, $this->calc->dividir(6, 3));
     }
 }
